e94863c2371b4e5071f12bc80317648b1558186e
José Bello <jfbello.work@gmail.com>
2025-09-18 11:00:23 +0200

    Lvl.1, Ex.4: refactored to use list functions, fixed EVEN meaning

--- lvl1_e04_list_operations.py
+++ lvl1_e04_list_operations.py
@@ -7,27 +7,22 @@ List Operations
         Create a new list with only the even numbers.
 """
 
-LISTA_INICIAL = [1,2,3,4,5,6,7,8,9,10]
-suma = 0
-largest = smallest = None
-only_even = []
+LISTA_INICIAL = [8,9,10,3,6,4,1,2,5,7]
 
+suma = 0
 for num in LISTA_INICIAL:
     suma += num
-    if not largest:
-        largest = num
-    if not smallest:
-        smallest = num
-    if num > largest:
-        largest = num
-    if num < smallest:
-        smallest = num
-    if  num % 2 != 0:
-        only_even.append(num)
 
 average = suma / len(LISTA_INICIAL)
+
+lista = LISTA_INICIAL.copy()
+lista.sort()
+smallest = lista[0]
+largest = lista[-1]
+
+only_even = [num for num in lista if num % 2 == 0]
 
 print(f"Lista Inicial: {LISTA_INICIAL}")
 print(f"La suma de los números es: {suma}, la media (average) es: {average}")
 print(f"El mayor número de la lista es: {largest}, el menor es: {smallest}")
-print(f"Los número impares de la lista: {only_even}")
+print(f"Los números pares de la lista: {only_even}")
